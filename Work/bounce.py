78c49a48d75fdd7e808c49213fc5b1233dec0602
Alvaro <alvarolukmanto@gmail.com>
2025-02-26 15:14:41 +0800

    feat: add bouncing ball exercise codes

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+next_bounce_multiplier = 3 / 5
+bounce_count = 1
+
+while (bounce_count <= 10):
+    height = height * next_bounce_multiplier
+    print(bounce_count, round(height, 4))
+    bounce_count = bounce_count + 1
