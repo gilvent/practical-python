4a9322b1240a68783c3b50a1e2e4dd9ab4097867
Alvaro <alvarolukmanto@gmail.com>
2025-02-27 14:16:04 +0800

    bugfix: exercise 1.11 fix overpayment in last month

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -15,19 +15,26 @@ def calculateWithExtraPayment(
     while principal > 0:
         month += 1
         payment = monthly_payment
+        principal_to_pay = principal * (1 + rate / 12)
 
+        # Add extra payment if any
         if month >= extra_payment_start_month and month <= extra_payment_end_month:
             payment = monthly_payment + extra_payment
 
-        principal = principal * (1 + rate / 12) - payment
+        # Last payment
+        if principal_to_pay <= payment:
+            payment = principal_to_pay
+
+        principal = principal_to_pay - payment
         total_payment += payment
         print(month, total_payment, principal)
 
-    return [round(total_payment, 2), month, principal]
+    return [round(total_payment, 2), month]
 
 
-[total_payment, months_required, principal] = calculateWithExtraPayment(500000.0, 0.05,2684.11, 61, 108, 1000)
+[total_payment, months_required] = calculateWithExtraPayment(
+    500000.0, 0.05, 2684.11, 61, 108, 1000
+)
 
-print('Total payment', total_payment)
-print('Months', months_required)
-
+print("Total payment", total_payment)
+print("Months", months_required)
