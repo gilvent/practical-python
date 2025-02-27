86a925f0cac77aa4bd8aa356cae0f762b3fd63a4
Alvaro <alvarolukmanto@gmail.com>
2025-02-27 13:55:05 +0800

    feat: exercise 1.9
    support custom period to calculate extra payments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,22 +1,29 @@
 # mortgage.py
 #
-# Exercise 1.8 Extra Payments
-
-def calculateWithExtraPayment (principal, rate, monthly_payment, first_year_extra_payment):
+# Exercise 1.9 Extra Payments on Custom Period
+def calculateWithExtraPayment(
+    principal,
+    rate,
+    monthly_payment,
+    extra_payment_start_month,
+    extra_payment_end_month,
+    extra_payment,
+):
     total_payment = 0.0
     month = 0
 
-    while (principal > 0):
+    while principal > 0:
         month += 1
         payment = monthly_payment
 
-        if (month <= 12):
-            payment = monthly_payment + first_year_extra_payment
+        if month >= extra_payment_start_month and month <= extra_payment_end_month:
+            payment = monthly_payment + extra_payment
 
         principal = principal * (1 + rate / 12) - payment
         total_payment += payment
-    
+
     return [round(total_payment, 2), month]
 
-[total_payment, months_required] = calculateWithExtraPayment(500000.0, 0.05, 2684.11, 1000)
+
+[total_payment, months_required] = calculateWithExtraPayment(500000.0, 0.05,2684.11, 61, 108, 1000)
 print(total_payment, months_required)
