6952c36ee47cb652670962886bf0458cdf6f7a99
Alvaro <alvarolukmanto@gmail.com>
2025-02-27 13:14:57 +0800

    feat: exercise 1.8
    add extra payment for the first 12 months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,14 +1,22 @@
 # mortgage.py
 #
-# Exercise 1.7
-def calculateMortgagePayment (principal, rate, montly_payment):
+# Exercise 1.8 Extra Payments
+
+def calculateWithExtraPayment (principal, rate, monthly_payment, first_year_extra_payment):
     total_payment = 0.0
+    month = 0
 
     while (principal > 0):
-        principal = principal * (1 + rate / 12) - montly_payment
-        total_payment += montly_payment
+        month += 1
+        payment = monthly_payment
+
+        if (month <= 12):
+            payment = monthly_payment + first_year_extra_payment
+
+        principal = principal * (1 + rate / 12) - payment
+        total_payment += payment
     
-    return round(total_payment, 2)
+    return [round(total_payment, 2), month]
 
-
-print('Total paid', calculateMortgagePayment(500000, 0.05, 2684.11))
+[total_payment, months_required] = calculateWithExtraPayment(500000.0, 0.05, 2684.11, 1000)
+print(total_payment, months_required)
