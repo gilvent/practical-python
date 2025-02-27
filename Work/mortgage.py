1116adabfb47ffa6555b9d30e46e8cdaa4201caa
Alvaro <alvarolukmanto@gmail.com>
2025-02-27 12:43:34 +0800

    feat: exercise 1.7
    calculate mortage example

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
 # mortgage.py
 #
 # Exercise 1.7
+def calculateMortgagePayment (principal, rate, montly_payment):
+    total_payment = 0.0
+
+    while (principal > 0):
+        principal = principal * (1 + rate / 12) - montly_payment
+        total_payment += montly_payment
+    
+    return round(total_payment, 2)
+
+
+print('Total paid', calculateMortgagePayment(500000, 0.05, 2684.11))
