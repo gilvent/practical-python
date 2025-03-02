9150ebb5a17edd551b1e492adef4af8a1474e008
Alvaro <alvarolukmanto@gmail.com>
2025-03-02 22:15:09 +0800

    feat: exercise 1.27 read a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+def calculatePortfolioCost():
+    with open('Data/portfolio.csv', 'rt') as file:
+        next(file)
+        total_price = 0
+
+        for row in file:
+            row_data = row.removesuffix('\n').split(',')
+            shares = int(row_data[1])
+            price = float(row_data[2])
+            total_price += shares * price
+        return total_price
+    
+cost = calculatePortfolioCost()
+print(cost)
