2021f26fedcfd2ddf2a36cf22edf605bf3c6b649
Alvaro <alvarolukmanto@gmail.com>
2025-03-02 22:37:11 +0800

    feat: exercise 1.31 add error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
-def portfolio_cost():
-    with open('Data/portfolio.csv', 'rt') as file:
+def portfolio_cost(path):
+    with open(path, 'rt') as file:
         next(file)
         total_price = 0
 
         for row in file:
             row_data = row.removesuffix('\n').split(',')
-            shares = int(row_data[1])
-            price = float(row_data[2])
-            total_price += shares * price
+            try:
+                shares = int(row_data[1])
+                price = float(row_data[2])
+                total_price += shares * price
+            except ValueError:
+                print(f'Price for {row_data[0]} cannot be calculated due to missing value')
         return total_price
     
-cost = portfolio_cost()
+cost = portfolio_cost('Data/missing.csv')
 print(cost)
