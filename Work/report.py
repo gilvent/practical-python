fea9f877c06fb77b8e90feb62d3b8e530c1c1749
Alvaro <alvarolukmanto@gmail.com>
2025-03-05 14:49:03 +0800

    feat: exercise 2.4 list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,14 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, "rt") as file:
+        rows = csv.reader(file)
+        next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
